6e2843ad987adf43ab9ee6780532f63e3647997d
semtio <semik20092@gmail.com>
2025-09-10 20:41:00 +0200

    en-In to en-IN

--- html-lang-switcher.php
+++ html-lang-switcher.php
@@ -71,7 +71,7 @@ final class Plugin
             'ru'    => 'ru',
             'es-ES' => 'es-ES',
             'es-AR' => 'es-AR',
-            'en-In' => 'en-In',
+            'en-IN' => 'en-IN',
             'it-IT' => 'it-IT',
             'de-DE' => 'de-DE',
         );
